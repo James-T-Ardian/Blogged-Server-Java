7584abcb9e4ab0a4298e31b8cd0a55b2ce9acdd2
James T Ardian <jamesardian01@gmail.com>
2023-07-15 22:49:18 -0700

    create user details for auth

--- src/main/java/com/bloggedserver/user/User.java
+++
@@ -1,7 +0,0 @@
-package com.bloggedserver.user;
-
-import jakarta.validation.constraints.NotNull;
-
-public record User(@NotNull(message = "User's username cannot be null") String username,
-                   @NotNull(message = "User's password cannot be null") String password) {
-}
